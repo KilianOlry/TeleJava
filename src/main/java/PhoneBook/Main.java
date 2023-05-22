504e7c6e6620cb86f3cb5cc52657525aec8ed8dd
Kilian Olry <kilian.olry@gmail.com>
2023-05-22 13:44:26 +0200

    Scanner refactor

--- src/main/java/PhoneBook/Main.java
+++ src/main/java/PhoneBook/Main.java
@@ -3,13 +3,23 @@ package PhoneBook;
 import java.util.Scanner;
 
 public class Main {
+
+    public static Scanner UserInputScanner;
+
     public static void main(String[] args)
     {
-        Scanner UserInputScanner = new Scanner(System.in);
+        UserInputScanner  = new Scanner(System.in);
 
-        System.out.println("Veuillez entrer votre nom");
-        String userLastName = UserInputScanner.nextLine();
-        System.out.println(userLastName);
+        String userLastName = getUserInput("Veuillez entrer votre Nom");
+        String userFirstName = getUserInput("Veuillez entrer votre Prénom");
+        String userPhoneNumber = getUserInput("Veuillez entrer votre Numéro");
 
     }
+        public static String getUserInput(String UserRequest)
+        {
+            System.out.println(UserRequest);
+            return UserInputScanner.nextLine();
+
+        }
+
 }
