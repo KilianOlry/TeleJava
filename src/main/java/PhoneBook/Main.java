34577b3cdbd48a01ec02241fa3a5fea8cca5c1be
Kilian Olry <kilian.olry@gmail.com>
2023-05-22 14:33:57 +0200

    Getter setter

--- src/main/java/PhoneBook/Main.java
+++ src/main/java/PhoneBook/Main.java
@@ -10,11 +10,21 @@ public class Main {
     {
         UserInputScanner  = new Scanner(System.in);
 
-        String userLastName = getUserInput("Veuillez entrer votre Nom");
-        String userFirstName = getUserInput("Veuillez entrer votre Prénom");
-        String userPhoneNumber = getUserInput("Veuillez entrer votre Numéro");
+        String userLastName = getUserInput("Veuillez entrer votre Nom :");
+        String userFirstName = getUserInput("Veuillez entrer votre Prénom :");
+        String userPhoneNumber = getUserInput("Veuillez entrer votre Numéro :");
 
+        Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
+
+
+        System.out.println(userLastName);
+        System.out.println(userFirstName);
+        System.out.println(userPhoneNumber);
+        System.out.println(newContact.toString());
     }
+
+
+
         public static String getUserInput(String UserRequest)
         {
             System.out.println(UserRequest);
